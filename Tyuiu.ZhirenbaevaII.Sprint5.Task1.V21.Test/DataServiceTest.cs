e1479af53bae5f0858a6ea118ce0e85d853ca9e4
KK <KK@DESKTOP-TIELPRS>
2023-11-19 21:07:25 +0500

    Task1

---
+++ Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21.Lib;
+
+using System.IO;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\Users\KK\source\repos\Tyuiu.ZhirenbaevaII.Sprint5\Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21\bin\Debug\OutPutFileTask1.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
+        
+    
+
---
+++ Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21.Lib;
+
+namespace Tyuiu.ZhirenbaevaII.Sprint5.Task1.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1";
+
+            Console.WriteLine("**");
+            Console.WriteLine(" Спринт #5                                                              ");
+            Console.WriteLine(" Тема: Обработка файлов                                                  ");
+            Console.WriteLine(" Задание #0                                                              ");
+            Console.WriteLine(" Вариант #18                                                            ");
+            Console.WriteLine(" Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1                     ");
+            Console.WriteLine("**");
+            Console.WriteLine("* УСЛОВИЕ:                                                                ");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до   *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("**");
+            int start = -5;
+            int end = 5;
+            Console.WriteLine("Начальная координата : " + start);
+            Console.WriteLine("Конечная координата : " + end);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(start, end);
+            Console.WriteLine("Файл :" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
+
+        
+    
+
