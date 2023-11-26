d5874ca1a00efb48efe6cf80d546908a922c6ccd
KK <KK@DESKTOP-TIELPRS>
2023-11-26 22:42:27 +0500

    Task6

---
+++ Tyuiu.ZhirenbaevaII.Sprint5.Task6.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZhirenbaevaII.Sprint5.Task6.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZhirenbaevaII.Sprint5.Task6.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("acf815b6-ca7f-4201-871d-1bc32a61e0f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZhirenbaevaII.Sprint5.Task6.V27/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZhirenbaevaII.Sprint5.Task6.V27.Lib;
+
+
+namespace Tyuiu.ZhirenbaevaII.Sprint5.Task6.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1";
+
+            Console.WriteLine("**");
+            Console.WriteLine(" Спринт #5                                                            ");
+            Console.WriteLine(" Тема: Обработка файлов                                                  ");
+            Console.WriteLine(" Задание #6                                                         ");
+            Console.WriteLine(" Вариант #27                                                      ");
+            Console.WriteLine(" Выполнила: Жиренбаева Ирина Ильгизовна | ИСТНб-23-1                     ");
+            Console.WriteLine("**");
+            Console.WriteLine("* УСЛОВИЕ:                                                                ");
+            Console.WriteLine("* Дан файл, в котором есть набор символьных данных.                       *");
+            Console.WriteLine("* Найти количество трехзначных чисел в заданной строке.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("*                                                                         *");
+           
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            string path = @"C:\DataSprint5\InPutDataFileTask6V27.txt";
+
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
